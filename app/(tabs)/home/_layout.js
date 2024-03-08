8189592efe37f5a48247bf44a58b9a9fba4aac2a
umergillani36 <syedomershahgillani@gmail.com>
2024-03-08 16:50:48 +0500

    home layout

---
+++ app/(tabs)/home/_layout.js
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+
+const Layout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="profile" />
+    </Stack>
+  );
+};
+
+export default Layout;
